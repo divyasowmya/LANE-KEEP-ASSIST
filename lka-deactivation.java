0103876170181a0ff5d0f7170d57c8b6460ce535
divyasowmya <158157685+divyasowmya@users.noreply.github.com>
2024-02-29 11:39:51 +0530

    #17048 lka-deactivation.java

---
+++ lka-deactivation.java
@@ -0,0 +1,12 @@
+public class LaneKeepAssist {
+    public static void main(String[] args) {
+        boolean driverTakingControl = true;
+        boolean driverChangingLanes = false;
+
+        if (driverTakingControl || driverChangingLanes) {
+            System.out.println("Deactivate Lane Keep Assist.");
+        } else {
+            System.out.println("Lane Keep Assist remains active.");
+        }
+    }
+}
